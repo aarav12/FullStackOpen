9e9b758cb08a8598971eed1263a70973dd20a60a
unknown <raghavprasad98@gmail.com>
2021-05-05 14:36:55 +0530

    uni-cafe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
 
-function App() {
+
+const Stats=(props)=>{
+  if(props.total===0){
+      return (
+          <h1>No feedback given</h1>
+      )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+   <p>good {props.good}</p>
+   <p>neutral {props.neutral}</p>
+   <p>bad {props.bad}</p>
+   <p>all {props.total}</p>
+   <p>averge {props.score}</p>
+   <p>positive{props.percent*100} </p>
+   </div>
+  )
 }
 
-export default App;
+const Button=({handleclick,text})=>{
+  return (
+    <button onClick={handleclick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  
+  const [neutral,setneutral]=useState(0);
+  const [good,setgood]=useState(0);
+  const [bad,setbad]=useState(0);
+ 
+  const handleneutral=()=>{
+    setneutral(neutral+1);
+  }
+  const handlegood=()=>{
+    setgood(good+1);
+  }
+  const handlebad=()=>{
+    setbad(bad+1);
+  }
+  var total=good+bad+neutral
+  var percent=good/total;
+  var score=(good-bad)/total
+  return (
+    <div>
+    <h1>Give feedback</h1>
+    
+    <Button handleclick={handleneutral} text="neutral"/>
+    <Button handleclick={handlegood} text="good"/>
+    <Button handleclick={handlebad} text="bad"/>
+
+     
+    <Stats good={good} bad={bad} neutral={neutral} percent={percent} score={score} total={total}/>
+
+    </div>
+  )
+
+}
+
+export default App
